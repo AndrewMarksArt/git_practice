48825bb2a6e5701ba11915f510362d175c05e1ac
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-27 17:51:46 -0700

    added a rot method to ornage class

--- main_project/test/oop_practice_orange.py
+++ main_project/test/oop_practice_orange.py
@@ -1,10 +1,14 @@
 # python oop practice, create an "Orange" class to instantiate Orange objects
 
 class Orange:
-    def __init__(self, weight: float, color: str) -> None:
+    def __init__(self, weight: float, color: str, mold=0) -> None:
         self.weight = weight
         self.color = color
+        self.mold = mold
         print(f"Created a {color} Orange weighing {weight} ounces.")
+
+    def rot(self, days, temp):
+        self.mold = days * temp
 
 
 orange1 = Orange(6, "dark orange")
@@ -15,3 +19,6 @@ orange1.weight = 10
 orange1.color = "light orange"
 
 print(orange1.color, orange1.weight)
+
+orange1.rot(2, 98.0)
+print(orange1.mold)
