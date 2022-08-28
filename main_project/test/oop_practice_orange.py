e68886e2c080ca3c58571a59fd61c9dd8354bb98
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-27 17:45:39 -0700

    created orange class, instantiated an orange, changed the weight and color

--- main_project/test/oop_practice_orange.py
+++ main_project/test/oop_practice_orange.py
@@ -1,8 +1,17 @@
 # python oop practice, create an "Orange" class to instantiate Orange objects
 
 class Orange:
-    def __init__(self) -> None:
-        print("created an orange!")
+    def __init__(self, weight: float, color: str) -> None:
+        self.weight = weight
+        self.color = color
+        print(f"Created a {color} Orange weighing {weight} ounces.")
 
 
-orange1 = Orange()
+orange1 = Orange(6, "dark orange")
+print(orange1.weight)
+print(orange1.color)
+
+orange1.weight = 10
+orange1.color = "light orange"
+
+print(orange1.color, orange1.weight)
