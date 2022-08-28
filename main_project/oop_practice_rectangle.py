8ba7addc2525475a738cb97935f0f96c1396b861
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-27 18:17:18 -0700

    added Circle, Triangle, and Hexagon classes to oop_practice_rectangle.py

--- main_project/oop_practice_rectangle.py
+++ main_project/oop_practice_rectangle.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Rectangle():
     def __init__(self, width, length) -> None:
         self.width = width
@@ -14,8 +17,46 @@ class Rectangle():
         self.length = length
 
 
+class Circle():
+    def __init__(self, radius) -> None:
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+
+class Triangle():
+    def __init__(self, height, base) -> None:
+        self.height = height
+        self.base = base
+
+    def area(self):
+        return (self.base * self.height) / 2
+
+
+class Hexagon():
+    def __init__(self, side_len) -> None:
+        self.side_len = side_len
+
+    def area(self):
+        return 6 * self.side_len
+
+
+# create rectangle object and print the area and perimeter
 rect = Rectangle(10, 20)
 print(rect.area(), rect.perimeter())
-
+# change the rectangle size and then print area and perimeter of new rectangle
 rect.change_size(20, 30)
 print(rect.area(), rect.perimeter())
+
+# Create a circle and find the area
+circle = Circle(5)
+print(circle.area())
+
+# create triangle and print area
+triangle = Triangle(4, 3)
+print(triangle.area())
+
+# create a hexagon and print the perimeter
+hexagon = Hexagon(3)
+print(hexagon.area())
