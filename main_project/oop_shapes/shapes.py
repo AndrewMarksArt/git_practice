fd218891cbe330c495eb21a6d38373958b6a4906
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-28 18:36:49 -0700

    added new classes and methods to shapes.py and added composition.py

--- main_project/oop_shapes/shapes.py
+++ main_project/oop_shapes/shapes.py
@@ -3,28 +3,47 @@ class Shape:
         self.width = w
         self.len = l
 
-    def print_size(self):
-        print(
-            """
-            {} wide by {} long
-            """.format(self.width, self.len)
-        )
+    def what_am_i(self):
+        print("I am a shape")
 
 
 class Square(Shape):
-    def area(self):
-        return self.width * self.len
+    def __init__(self, side_len) -> None:
+        self.side_len = side_len
 
-    def print_size(self):
+    def change_size(self, new_len):
+        self.side_len = self.side_len + new_len
+
+    def calc_perimeter(self):
+        return self.side_len * 4
+
+    def what_am_i(self):
+        print(f"I am a square with a side length of {self.side_len}")
+
+
+class Rectangle(Shape):
+    def __init__(self, w, l) -> None:
+        super().__init__(w, l)
+
+    def calc_perimeter(self):
+        return 2 * self.width + 2 * self.len
+
+    def what_am_i(self):
         print(
-            """
-            I am {} wide by {} long
-            """.format(self.width, self.len)
+            f"I am a rectangle with a width of {self.width}"
+            + " and a length of {self.len}"
         )
 
 
-shape1 = Shape(20,20)
-shape1.print_size()
+shape1 = Shape(20, 20)
+shape1.what_am_i()
 
-square1 = Square(20, 20)
-square1.print_size()
+square1 = Square(4)
+square1.what_am_i()
+print(square1.calc_perimeter())
+square1.change_size(10)
+print(square1.calc_perimeter())
+
+rect1 = Rectangle(10, 5)
+rect1.what_am_i()
+print(rect1.calc_perimeter())
