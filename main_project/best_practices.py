537dbbb7f9244834c0300654517aa83d312f9923
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-08-28 19:28:16 -0700

    updated best practices with main() function and added more methods

--- main_project/best_practices.py
+++ main_project/best_practices.py
@@ -5,7 +5,28 @@ print("this is my file to demonstrate best practices.")
 
 def process_data(data):
     print("beginning data processing")
-    modified_data = data + " modified data"
+    modified_data = data + " is now modified data"
     sleep(3)
     print("data processing finished")
     return modified_data
+
+
+def read_data_from_web():
+    print("Reading data from the web")
+    data = "Data from the web"
+    return data
+
+
+def write_data_to_db(data):
+    print("Writing data to a database")
+    print(data)
+
+
+def main():
+    data = read_data_from_web()
+    modified_data = process_data(data)
+    write_data_to_db(modified_data)
+
+
+if __name__ == "__main__":
+    main()
